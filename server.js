c8f85edc2e36b685e5222a99ffb3d3ffb097b29d
Sac6567 <sac6567@gmail.com>
2022-08-30 00:41:05 +0530

    Cleaning Code

--- server.js
+++ server.js
@@ -1,17 +1,13 @@
 const dotenv = require('dotenv');
 
-dotenv.config({path:'./config.env'});
+dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
-// console.log(process.env);
-
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`App running on port: ${port}...`);
+  console.log(`App running on port: ${port}...`);
 });
-
-
 
 // alt + shift + F
 //  npm i eslint prettier eslint-config-prettier eslint-plugin-prettier eslint-config-airbnb eslint-plugin-node eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-react --save-dev
